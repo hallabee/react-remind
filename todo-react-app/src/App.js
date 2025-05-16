98183fd2454665e90d91eb87107833429bf61a79
hallabee <wkdwkxogh@naver.com>
2025-05-16 16:08:21 +0900

    React Remind Day01

---
+++ todo-react-app/src/App.js
@@ -0,0 +1,42 @@
+//import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo';
+import React, {useState} from 'react';
+import { Container, List, Paper } from '@mui/material';
+import AddTodo from './AddTodo';
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true
+    }
+  ]);
+
+  let todoItems =items.length > 0 && (
+    <Paper style={{margin: 16}}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id}/>
+        ))}
+      </List>
+    </Paper>
+  );
+
+  return (
+    <div className='App'>
+      <Container maxWidth="md">
+        <AddTodo/>
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
