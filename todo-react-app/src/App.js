0a98d49f44028ba5e5f3f126ae7598d2b83d6ec9
hallabee <wkdwkxogh@naver.com>
2025-05-29 16:10:56 +0900

    React Remind Day02

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -6,24 +6,32 @@ import { Container, List, Paper } from '@mui/material';
 import AddTodo from './AddTodo';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: "0",
-      title: "Hello World 1",
-      done: true
-    },
-    {
-      id: "1",
-      title: "Hello World 2",
-      done: true
-    }
-  ]);
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    item.id = "ID-" + items.length; //key를 위한 id
+    item.done = false; //done 초기화
+    //업데이트는 반드시 setItems로 하고 새 배열을 만들어야 한다
+    setItems([...items, item]);
+    console.log("items : ", items);
+  };
+
+  const deleteItem = (item) => {
+    //삭제할 아이템 찾기
+    const newItems = items.filter(e => e.id !== item.id);
+    // 삭제할 아이템을 제외한 아이템 재배열 후 저장
+    setItems([...newItems]);
+  }
+
+  const editItem = () => {
+    setItems([...items]);
+  }
 
   let todoItems =items.length > 0 && (
     <Paper style={{margin: 16}}>
       <List>
         {items.map((item) => (
-          <Todo item={item} key={item.id}/>
+          <Todo item={item} key={item.id} deleteItem={deleteItem} editItem={editItem}/>
         ))}
       </List>
     </Paper>
@@ -32,7 +40,7 @@ function App() {
   return (
     <div className='App'>
       <Container maxWidth="md">
-        <AddTodo/>
+        <AddTodo addItem={addItem}/>
         <div className='TodoList'>{todoItems}</div>
       </Container>
     </div>
--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,14 +1,43 @@
 import React, {useState} from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from '@mui/material';
+import { ListItem, ListItemText, InputBase, Checkbox, IconButton } from '@mui/material';
+import { DeleteOutlined } from '@mui/icons-material';
 
 const Todo = (props) => {
     const [item, setItem] = useState(props.item);
+    const deleteItem = props.deleteItem;
+    //deleteEventHandler
+    const deleteEventHandler = () => {
+        deleteItem(item);
+    };
+    const [readOnly, setReadOnly] = useState(true);
+    //turnOffReadOnly
+    const turnOffReadOnly = () => {
+        setReadOnly(false);
+    };
+    //turnOnReadOnly
+    const turnOnReadOnly = (e) => {
+        if(e.key === "Enter"){
+            setReadOnly(true);
+        }
+    };
+    const editItem = props.editItem;
+    const editEventHandler = (e) => {
+        item.title = e.target.value;
+        editItem();
+    };
+    const checkboxEventHandler = (e) => {
+        item.done = e.target.checked;
+        editItem();
+    }
     return(
         <ListItem>
-            <Checkbox checked={item.done}/>
+            <Checkbox checked={item.done} onChange={checkboxEventHandler}/>
             <ListItemText>
                 <InputBase
-                inputProps={{"aria-label" : "naked"}}
+                inputProps={{"aria-label" : "naked", readOnly: readOnly}}
+                onClick={turnOffReadOnly}
+                onKeyDown={turnOnReadOnly}
+                onChange={editEventHandler}
                 type='checkbox'
                 id={item.id}
                 name={item.id}
@@ -17,6 +46,14 @@ const Todo = (props) => {
                 fullWidth={true}
                 />
             </ListItemText>
+            <ListItem
+                secondaryAction={
+                    <IconButton aria-label='Delete Todo' onClick={deleteEventHandler}>
+                        <DeleteOutlined />
+                    </IconButton>
+                }
+            >
+            </ListItem>
         </ListItem>
     );
 };
